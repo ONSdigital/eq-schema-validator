79cbb5af703990eb0bc23a920cbb4cc85ef92258
Mebin Abraham <mebin.abraham@qa.com>
2018-04-19 16:18:54 +0100

    Check for bracket notation

--- app/validation/validator.py
+++ app/validation/validator.py
@@ -78,22 +78,15 @@ class Validator:
             return '{}'.format(e)
 
     def _validate_schema_contain_required_metadata(self, schema):
-        # Ensure that metadata used within the schema are implicitly defined in required_metadata field
+        # Ensure that metadata used within the schema are explicitly defined.
 
         errors = []
-        default_metadata = ['user_id', 'period_id']
 
-        required_metadata = schema['required_metadata']
+        default_metadata = ['user_id', 'period_id']  # Metadata that must be present in all schemas
+        required_metadata = schema['required_metadata']  # Metadata required within the schema.
 
-        # This regex will find all words that precede with "metadata" then a "[" or "." following by an optional "'"
+        # Find all words that precede with "metadata['"
         parsed_metadata = re.findall(r"(?<=metadata\[\')\w+", str(schema))
-
-        parsed_metadata1 = re.findall(r"(?<=metadata\.)\w+", str(schema))
-        if parsed_metadata1:
-            int('t')
-        # (?<=[\|\{\,\ \(]metadata)\.(\w+)(?=[\ \|\}\)\,])
-
-        print('\033[93m', set(parsed_metadata), '\x1b[0m')
 
         for metadata in set(parsed_metadata):
             if metadata not in required_metadata:
@@ -102,6 +95,7 @@ class Validator:
         for metadata in required_metadata:
             if metadata not in set(parsed_metadata) and metadata not in default_metadata:
                 errors.append(self._error_message('Unused metadata defined in required_metadata - {}'.format(metadata)))
+
         return errors
 
     def validate_calculated_ids_in_answers_to_calculate_exists(self, question):
