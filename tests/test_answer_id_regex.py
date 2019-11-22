7054b5d49794245ba45f88aa39c30bda66d4f9d1
Ian Wootten <hi@niftydigits.com>
2019-11-22 12:09:49 +0000

    Format against latest black

--- tests/test_answer_id_regex.py
+++ tests/test_answer_id_regex.py
@@ -11,19 +11,19 @@ def create_schema_with_answer_id(answer_id):
     :param answer_id: The Id to use for the answer.
     :return: The JSON file with the Id swapped for schema_id
     """
-    schema_path = 'tests/schemas/valid/test_schema_id_regex.json'
+    schema_path = "tests/schemas/valid/test_schema_id_regex.json"
 
-    with open(schema_path, encoding='utf8') as json_data:
+    with open(schema_path, encoding="utf8") as json_data:
         json_content = json.load(json_data)
 
-        json_content['sections'][0]['groups'][0]['blocks'][0]['question']['answers'][0]['id'] = answer_id
+        json_content["sections"][0]["groups"][0]["blocks"][0]["question"]["answers"][0][
+            "id"
+        ] = answer_id
         return json_content
 
 
 def test_valid_answer_ids():
-    answer_ids = [
-        'star-wars', 'name-with-hyphens', 'this-is-a-valid-id-0', 'answer'
-    ]
+    answer_ids = ["star-wars", "name-with-hyphens", "this-is-a-valid-id-0", "answer"]
 
     for answer_id in answer_ids:
         json_to_validate = create_schema_with_answer_id(answer_id)
@@ -34,7 +34,10 @@ def test_valid_answer_ids():
 
 def test_invalid_answer_ids():
     answer_ids = [
-        '!n0t-@-valid-id', 'NOT-A-VALID-ID', 'not_a_valid_id', 'not a valid id'
+        "!n0t-@-valid-id",
+        "NOT-A-VALID-ID",
+        "not_a_valid_id",
+        "not a valid id",
     ]
 
     for answer_id in answer_ids:
@@ -43,4 +46,4 @@ def test_invalid_answer_ids():
 
         expected_message = f"'{answer_id}' does not match"
 
-        assert expected_message in schema_errors.get('predicted_cause')
+        assert expected_message in schema_errors.get("predicted_cause")
--- tests/test_validate_schema.py
+++ tests/test_validate_schema.py
@@ -6,10 +6,13 @@ from app.validation.validator import Validator
 
 
 def test_schema():
-    with open('schemas/questionnaire_v1.json', encoding='utf8') as schema_data:
+    with open("schemas/questionnaire_v1.json", encoding="utf8") as schema_data:
         schema = json.load(schema_data)
-        resolver = RefResolver(base_uri='https://eq.ons.gov.uk/', referrer=schema,
-                               store=Validator.lookup_ref_store())
+        resolver = RefResolver(
+            base_uri="https://eq.ons.gov.uk/",
+            referrer=schema,
+            store=Validator.lookup_ref_store(),
+        )
 
         validator = validators.validator_for(schema)
         validator.resolver = resolver
